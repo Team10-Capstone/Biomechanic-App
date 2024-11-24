a5c16d94b0dbc854bea6af29597eda5023d792e7
Dylan Norris <114193369+dyl-norris19@users.noreply.github.com>
2024-11-24 16:50:18 -0600

    Simple Buttons Created
    Canvas Overlay added, along with buttons that print to the console they've been clicked. In the future the UI would have to change and the buttons would need to do what they say.

---
+++ Assets/Scripts/RecordingButton.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.UI; // Import the UI namespace to access UI elements like Button
+
+public class RecordingButton : MonoBehaviour
+{
+    public Button button; // Reference to the UI Button
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Ensure the button reference is set
+        if (button != null)
+        {
+            button.onClick.AddListener(OnButtonClick); // Register the button click event
+        }
+        else
+        {
+            Debug.LogWarning("Button reference not assigned.");
+        }
+    }
+
+    // This function is called when the button is clicked
+    void OnButtonClick()
+    {
+        Debug.Log("Recording Clicked"); // Log the message when the button is clicked
+    }
+}
