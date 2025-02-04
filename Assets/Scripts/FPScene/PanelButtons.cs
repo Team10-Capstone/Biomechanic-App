165690674e72c340375b2abb3f6e03592602ccb5
Dylan Norris <114193369+dyl-norris19@users.noreply.github.com>
2025-02-04 00:00:42 -0600

    User Profile Panel fleshed out, new panel for Editing User Profile
    The UP Panel now has spots for information like Name, ID, etc. to go. Edit button opens new panel where the text fields are now editable. nothing can be saved yet.

--- Assets/Scripts/FPScene/PanelButtons.cs
+++ Assets/Scripts/FPScene/PanelButtons.cs
@@ -6,19 +6,34 @@ public class UIController : MonoBehaviour
     public GameObject forkPanel;       // Panel with the user profile button
     public GameObject userProfilePanel; // Panel for the user profile
 
+    public GameObject userProfileEditPanel;
+
     public Button userProfileButton; // Button to open the user profile
+
+    public Button backButton;
+
+    public Button editButton;
+
+    public Button cancelButton;
 
     void Start()
     {
         // Ensure only the fork panel is active initially
         forkPanel.SetActive(true);
         userProfilePanel.SetActive(false);
+        userProfileEditPanel.SetActive(false);
 
         if (userProfileButton != null)
             userProfileButton.onClick.AddListener(OpenUserProfile);
 
-        // if (backButton != null)
-        //     backButton.onClick.AddListener(CloseUserProfile);
+        if (backButton != null)
+            backButton.onClick.AddListener(CloseUserProfile);
+
+        if (editButton != null)
+            editButton.onClick.AddListener(OpenUserProfileEdit);
+
+        if (cancelButton != null)
+            cancelButton.onClick.AddListener(CloseUserProfileEdit);
     }
 
     public void OpenUserProfile()
@@ -31,5 +46,17 @@ public class UIController : MonoBehaviour
     {
         userProfilePanel.SetActive(false); // Hide user profile panel
         forkPanel.SetActive(true);         // Show fork panel again
+    }
+
+    public void OpenUserProfileEdit()
+    {
+        userProfilePanel.SetActive(false);
+        userProfileEditPanel.SetActive(true);
+    }
+
+    public void CloseUserProfileEdit()
+    {
+        userProfileEditPanel.SetActive(false);
+        userProfilePanel.SetActive(true);
     }
 }
